865abb4de98d591fa493967d3d56bf795db88569
ashrith-devison <158307747+ashrith-devison@users.noreply.github.com>
2024-10-23 01:49:07 +0530

    Updated the Setup

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","admin","yvxqnzzwfpexbuin","project-ba","3310");
-    $MailLoginData = array(
-        "mailid" => "apd.vogb32@gmail.com",
-        "mailpasskey" => "yvxqnzzwfpexbuin", //yvxq nzzw fpex buin
-    );
-    
-?>  
